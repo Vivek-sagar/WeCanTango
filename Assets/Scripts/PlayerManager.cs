ca0e96a35da4923aafde9f59131d5d30ec37b7f1
nigelDaMan <pokemonjohto4@gmail.com>
2015-10-19 20:37:46 -0700

    Found the bug with the hashtable, needed to use return to bump out of the entire function, not a break. The break had me just out of the inner most loop structure so ended up trying to add somethign of the same key twice

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -61,7 +61,13 @@ public class PlayerManager : MonoBehaviour
 		mybiome = biome.biomeMap [chunkx, chunkz];
 		/*if (mybiome != BIOMES.sand)
 			return;*/
+
+		/*OPTIMIZE THIS LATER, make it nlog(n) at least !!!!!!!!!!!!!!!
+		 Mergesort
+		 quicksort
+		 COME ON MAN!!*/
 		for (int i =0; i<directions.Length; i++) {
+			//Gets the Chunk at my position
 			chunk = vxe.getChunkFromPt (playerTrans.position + directions [i]);
 			Vec3Int chunkVXCoords = vxe.getChunkCoords (playerTrans.position + directions [i]);
 			if (chunk == null)
@@ -77,9 +83,10 @@ public class PlayerManager : MonoBehaviour
 
 	void PullSpawnObject (Vector3 chunkBaseCoords, Vec3Int chunkVXCoords, Chunks chunk)
 	{
+		//Vector3 chunkWorldCoord = vxe.FromGrid (chunkVXCoords) / vxe.chunk_size;
 		if (desertEnvironment.Count > 0) {
 			GameObject gbj = desertEnvironment [0];
-
+			bool hasKey = desertTable.ContainsKey (chunkVXCoords);
 			//Loop thru each voxel in the chunk
 			for (int x=0; x<vxe.chunk_size; x++)
 				for (int z=0; z<vxe.chunk_size; z++)
@@ -87,13 +94,15 @@ public class PlayerManager : MonoBehaviour
 						Voxel vx = chunk.getVoxel (new Vec3Int (x, y, z));
 						//if vx is occ and has a sruface, spawn on top
 						if (vx.isOccupied () && vxe.voxelHasSurface (vx, VF.VX_TOP_SHOWN)) {
-							Vector3 voxelCoords = vxe.FromGridUnTrunc (chunkBaseCoords + new Vector3 (x, y, z));
-						
+
+							//Vector3 voxelCoords = chunkBaseCoords + new Vector3 (x, y, z) / vxe.voxel_size;
+							Vector3 voxelCoords = chunkBaseCoords + new Vector3 (x, y, z) * vxe.voxel_size;//vxe.FromGrid (new Vec3Int (x, y, z));
 							gbj.GetComponent<Transform> ().position = voxelCoords;
 							gbj.SetActive (true);
 							desertEnvironment.RemoveAt (0);
 							desertTable.Add (chunkVXCoords, gbj);
-							break;
+							//Debug.Log (string.Format ("ChunkWorldCoord {4} VoxelGridCoord ({1} ,{2}, {3} VoxelWorldCoord {0}  )", voxelCoords, x, y, z, chunkWorldCoord));
+							return;
 						}
 						//Keep THIS here so less Loops each frame
 						
