f6755c547ff0b2e8386ab75a61cd34170f733812
nigelDaMan <pokemonjohto4@gmail.com>
2015-11-04 19:14:39 -0800

    balrghe trying to get this things to works

--- Assets/Scripts/IntroTutorialManager.cs
+++ Assets/Scripts/IntroTutorialManager.cs
@@ -8,6 +8,7 @@ public class IntroTutorialManager : MonoBehaviour
 	{
 		Wait,
 		PocketWatchSwing,
+        PreGaze,
 		SheepGaze,
 		OpenPortal,
 		Finish,
@@ -23,6 +24,8 @@ public class IntroTutorialManager : MonoBehaviour
 	{
 		tutorialPhase = TutorialPhase.Wait;
 		playerGazeScript = GameObject.FindGameObjectWithTag ("Player").GetComponent<TutorialGaze> ();
+        SetMeshRenderersInChildren(tutorialSheep, false);
+        SetMeshRenderersInChildren(gazeTutorialGameObjects, false);
 	}
 	
 	// Update is called once per frame
@@ -31,39 +34,66 @@ public class IntroTutorialManager : MonoBehaviour
 		if (gazeCount > 2)
 			tutorialPhase = TutorialPhase.OpenPortal;
 		  
-		switch (tutorialPhase) {
-		case TutorialPhase.Wait:	
-			break;
-		case TutorialPhase.PocketWatchSwing:
+	
+		if(tutorialPhase== TutorialPhase.Wait){
+            return;
+        }
+		else if(tutorialPhase==  TutorialPhase.PocketWatchSwing){
 
-			//Disable PocketWatch
-			pocketWatch.SetActive (false);
+            //Disable PocketWatch
+            SetMeshRenderersInChildren(pocketWatch, false);
 
-			//After Pocket Watch Swing is done, allow the TutorialSheep and TutorialGaze 
-			//script to start doing stuff
-			sheepScript.waitForAnimationEnd = false;
-			sheepScript.ChangeTarget (gazeTargets [gazeCount]);
-			playerGazeScript.waitForAnimationEnd = false;
-			gazeTutorialGameObjects.SetActive (true);
-			tutorialPhase = TutorialPhase.SheepGaze;
-			break;
-		case TutorialPhase.SheepGaze:			
-			WaitForGaze ();
-			break;
-		case TutorialPhase.OpenPortal:
-			tutorialPhase = TutorialPhase.Finish;
-			break;
-		case TutorialPhase.Finish:
-			break;
-		}
+            //After Pocket Watch Swing is done, allow the TutorialSheep and TutorialGaze 
+            //script to start doing stuff
+            sheepScript.waitForAnimationEnd = false;
+            playerGazeScript.waitForAnimationEnd = false;
 
+            //Set the Sheep's Gaze Target
+            sheepScript.ChangeTarget(gazeTargets[gazeCount]);
+           
+            tutorialPhase = TutorialPhase.SheepGaze;
+        }
+		else if(tutorialPhase== TutorialPhase.SheepGaze){
+            WaitForGaze ();
+        }			
+		else if(tutorialPhase== TutorialPhase.OpenPortal){	
+            tutorialPhase = TutorialPhase.Finish;
+        }
+		
 	}
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="phase"></param>
 	public void SetTutorialPhase (TutorialPhase phase)
 	{
 		tutorialPhase = phase;
 	}
 
+
+    /// <summary>
+    /// BLASAEOILAHEAEU Unity wont let me use any public function for animation events ~~~!!
+    /// 
+    /// </summary>
+    public void SetSheepOn()
+    {
+        SetMeshRenderersInChildren(tutorialSheep, true);
+    }
+
+    public void SetMeshRenderersInChildren(GameObject parent, bool state)
+    {
+        MeshRenderer[] renders = parent.GetComponentsInChildren<MeshRenderer>();
+
+        foreach (MeshRenderer child in renders)
+        {
+            child.enabled = state;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
 	public void WaitForGaze ()
 	{
 		playerGazeScript.runGaze = sheepScript.atGazeTarget;
